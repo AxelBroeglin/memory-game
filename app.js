f68bb4830b6df13299288a4d8d2649fd534e62be
Axel Broeglin <axelbroeglin@gmail.com>
2022-03-10 09:21:21 +0100

    Matches ok, event listener removed for matches

--- app.js
+++ app.js
@@ -56,6 +56,9 @@ cardArray.sort(() => 0.5 - Math.random())
 
 //Selecte Grid with its #id
 const gridDisplay = document.querySelector('#grid')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 //Function to create the board
 function createBoard () {
@@ -72,6 +75,38 @@ function createBoard () {
 
 createBoard()
 
-function flipCard(){
-
+function checkMatch (){
+const cards = document.querySelectorAll('img')
+const optionOneId = cardsChosenIds[0]
+const optionTwoId = cardsChosenIds[1]
+if (optionOneId == optionTwoId){
+  alert('You clicked the same card !')
 }
+
+  console.log('check for a match')
+  if (cardsChosen[0] == cardsChosen[1]){
+    alert('You found a match')
+    cards[optionOneId].setAttribute('src', 'images/white.png')
+    cards[optionTwoId].setAttribute('src', 'images/white.png')
+    cards[optionOneId].removeEventListener ('click', flipCard)
+    cards[optionTwoId].removeEventListener ('click', flipCard)
+    cardsWon.push(cardsChosen)
+  }
+  cardsChosen = []
+  cardsChosenIds = []
+}
+
+function flipCard(){
+  console.log(cardArray)
+  //This lets us interact with whatever element we click
+  const cardId = this.getAttribute('data-id')
+  cardsChosen.push(cardArray[cardId].name)
+  cardsChosenIds.push(cardId)
+  console.log(cardsChosen)
+  console.log(cardsChosenIds)
+  this.setAttribute('src', cardArray[cardId].img)
+  if (cardsChosen.length === 2) {
+  setTimeout( checkMatch, 500)
+}
+}
+
